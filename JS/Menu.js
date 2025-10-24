79f743203cc6a527051cc9f880c51e8897c540b2
Andrea Marco Giaccherini <amgiaccherini@gmail.com>
2025-10-24 18:06:37 +0200

    maybe ultimate fix for safari uncontrollable scroll

--- JS/Menu.js
+++ JS/Menu.js
@@ -1,15 +1,28 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
-const body = document.querySelector('body');
+const body = document.body;
 const animation = document.getElementById('nav-icon3');
 
-var shown = true;
+let shown = true;
+let scrollY = 0;
 
 hamburger.addEventListener('click', () => {
 
     menu.classList.toggle('active');
     animation.classList.toggle('open');
-    body.style.overflowY = (shown)?"hidden":"auto";
-    body.style.overscrollBehaviorY = (shown)?"none":"";
+    
+    if(shown) {
+        scrollY = window.scrollY;
+        body.style.position = 'fixed';
+        body.style.top = `-${scrollY}px`;
+        body.style.width = '100%';
+        body
+    } else {
+        body.style.position = '';
+        body.style.top = '';
+        body.style.width = '';
+        window.scrollTo(0, scrollY);
+    }
+
     shown=!shown;
 });
