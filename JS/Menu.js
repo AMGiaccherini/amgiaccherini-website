96feb026c3192b235ef5b9896855b8c5135c5b13
Andrea Marco Giaccherini <amgiaccherini@gmail.com>
2025-10-24 19:36:30 +0200

    1024px width fixies (and some of 768px)

--- JS/Menu.js
+++ JS/Menu.js
@@ -15,11 +15,9 @@ hamburger.addEventListener('click', () => {
         scrollY = window.scrollY;
         body.style.position = 'fixed';
         body.style.top = `-${scrollY}px`;
-        body.style.width = '100%';
     } else {
         body.style.position = '';
         body.style.top = '';
-        body.style.width = '';
         window.scrollTo(0, scrollY);
     }
 
